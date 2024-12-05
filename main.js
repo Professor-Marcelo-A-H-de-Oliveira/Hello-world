2feeab5fc286f9978a3c14d63eba61854da4db63
Professor-Marcelo-A-H-de-Oliveira <marceloadrianooliveira@prof.educacao.sp.gov.br>
2024-12-05 14:46:16 -0300

    Finalização do Projeto
    Fim do projeto

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let nomeUsuário = "";
 let elemento = document.querySelector("#nome-usuário");
 
-while (nomeUsuário == ""){
+while(nomeUsuário == ""){
     nomeUsuário = prompt("Qual o seu nome?");
 }
 
-if (nomeUsuário == null){
-    elemento.textContent = 'seja muito bem vindo.';
+if(nomeUsuário == null){
+    elemento.textContent = 'seja muito bem-vindo.';
 }else{
     elemento.textContent = nomeUsuário;
 }
